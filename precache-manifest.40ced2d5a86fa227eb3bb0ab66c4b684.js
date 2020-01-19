f361cf6584151c90d513bfb9815ddffc180a39a4
Keshav Kaushik <keshav.k@Apples-MacBook-Pro.local>
2020-01-19 21:43:50 +0530

    Updates

---
+++ precache-manifest.40ced2d5a86fa227eb3bb0ab66c4b684.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "430f806921691c4b825a00aaad45bda7",
+    "url": "/kaukeshav/React-Assessment/index.html"
+  },
+  {
+    "revision": "9cef148524654d3590e6",
+    "url": "/kaukeshav/React-Assessment/static/css/main.ed0d3f1d.chunk.css"
+  },
+  {
+    "revision": "24d32d813b220611dc6c",
+    "url": "/kaukeshav/React-Assessment/static/js/2.afa76da1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/kaukeshav/React-Assessment/static/js/2.afa76da1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9cef148524654d3590e6",
+    "url": "/kaukeshav/React-Assessment/static/js/main.c01e0c7f.chunk.js"
+  },
+  {
+    "revision": "ee69e5351e89f71d1d0a",
+    "url": "/kaukeshav/React-Assessment/static/js/runtime-main.e7916a6f.js"
+  }
+]);
